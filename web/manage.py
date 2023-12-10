29ce31205ef9cf7cd927f4c401a0c2f3941204bf
luca <lucapalonca@gmail.com>
2023-12-10 16:39:50 +0100

    Remove prints

--- web/manage.py
+++ web/manage.py
@@ -7,8 +7,6 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-    print(f"DB: {os.environ.get('POSTGRES_USER')}")
-    print(f"DB PWD: {os.environ.get('POSTGRES_PASSWORD')}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
