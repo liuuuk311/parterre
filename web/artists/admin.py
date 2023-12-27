7576a04cf9696385c3d24c951344451c7fa845af
luca <lucapalonca@gmail.com>
2023-12-27 14:06:24 +0100

    Show tracks on admin tool

--- web/artists/admin.py
+++ web/artists/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin, messages
-
-from artists.models import Artist, Profile
+from django import forms
+from artists.models import Artist, Profile, ArtistPopularity, Track
 from artists.tasks import import_artist_data, import_top_tracks
 
 
@@ -9,9 +9,32 @@ class ArtistProfileInline(admin.TabularInline):
     extra = 1
 
 
+class ArtistPopularityInline(admin.TabularInline):
+    model = ArtistPopularity
+    can_delete = False
+    extra = 0
+
+    verbose_name_plural = "Popularity"
+
+    readonly_fields = (
+        "spotify_popularity",
+        "spotify_followers",
+        "monthly_listeners",
+        "parterre_score",
+    )
+
+
+class TrackInline(admin.TabularInline):
+    model = Track.artists.through
+    extra = 0
+
+    def get_readonly_fields(self, request, obj=None):
+        return [field.name for field in self.model._meta.fields]
+
+
 class ArtistAdmin(admin.ModelAdmin):
     fields = ('spotify_url', 'stage_name', 'image', 'bio', 'force_visible')
-    inlines = [ArtistProfileInline]
+    inlines = [ArtistProfileInline, ArtistPopularityInline, TrackInline]
     actions = ['import_artist_data_from_spotify', 'import_top_tracks_from_spotify']
 
     def import_artist_data_from_spotify(self, request, queryset):
